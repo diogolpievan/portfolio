91075d4a360ca7639d7f5e1d592fcb84403b5a04
Diogo Louvo Pievan <diogo.pievan@gmail.com>
2025-09-19 18:05:27 -0300

    feat: footer finalized

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,15 @@
+import { Logo } from "../logo";
+
+export const Footer = () => {
+  return (
+    <footer className="flex justify-items-center w-full ">
+      <div className="container w-full flex flex-col md:flex-row justify-center items-center md:justify-between lg:px-40">
+        <Logo />
+        <div className="text-[#666666] mt-2 md:mt-0">
+          <p>&lt; Projeto por Diogo Louvo Pievan /&gt;</p>
+          <p>Todos os direitos reservados © 2025</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
