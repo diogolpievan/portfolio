d6fee7ea53671b3965d49c9334b0f8a6e66b2431
Diogo Louvo Pievan <diogo.pievan@gmail.com>
2025-09-19 13:42:49 -0300

    chore: add logo component

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,18 +1,13 @@
 "use client";
 
-import Link from "next/link";
 import { Navbar } from "./navbar";
+import { Logo } from "../logo";
 
 export const Header = () => {
   return (
     <header className="w-full flex justify-items-center sticky top-0 z-50 bg-background">
       <div className="container flex justify-center md:justify-between lg:px-40">
-        <Link href={"/"}>
-          <h2 className="font-bold text-2xl">
-            Diogo L<span className="text-emerald-400">.</span> Pievan
-          </h2>
-        </Link>
-
+        <Logo />
         <Navbar />
       </div>
     </header>
