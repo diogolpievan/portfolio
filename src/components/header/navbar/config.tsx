fd7547ba40d33da1add726b4ba270062ef461264
Diogo Louvo Pievan <diogo.pievan@gmail.com>
2025-09-19 18:45:01 -0300

    feat: configs dropdown menu

---
+++ src/components/header/navbar/config.tsx
@@ -0,0 +1,47 @@
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+} from "@/components/ui/dropdown-menu";
+import { GoGear } from "react-icons/go";
+import Image from "next/image";
+
+export const Config = () => {
+  return (
+    <DropdownMenu modal={false}>
+      <DropdownMenuTrigger asChild>
+        <GoGear
+          size={22}
+          className="hover:text-emerald-400 transition-colors duration-300 hover:text-shadow-2xs cursor-pointer data-[state=open]:text-emerald-400"
+        />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="max-h-52 bg-primary-foreground px-4 rounded-4xl mt-3">
+        <div className="container flex flex-col gap-4">
+          <div>
+            <h4>Theme</h4>
+            <input type="checkbox" name="" id="" />
+          </div>
+          <div className="flex flex-col gap-2">
+            <h4>Language</h4>
+            <div className="flex flex-row w-full justify-between items-center ">
+              <Image
+                src="https://upload.wikimedia.org/wikipedia/commons/4/4a/Brazilian_flag_icon_round.svg"
+                alt="Portuguese"
+                width={36}
+                height={36}
+                className="border-2 border-sidebar-border rounded-full cursor-pointer hover:border-emerald-400 transition-all duration-300 shadow-neon"
+              />
+              <Image
+                src="https://upload.wikimedia.org/wikipedia/commons/1/13/United-kingdom_flag_icon_round.svg"
+                alt="English"
+                width={36}
+                height={36}
+                className="border-2 border-sidebar-border rounded-full cursor-pointer hover:border-emerald-400 transition-all duration-300 shadow-neon"
+              />
+            </div>
+          </div>
+        </div>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
--- src/components/header/navbar/index.tsx
+++ src/components/header/navbar/index.tsx
@@ -1,3 +1,5 @@
+import { Button } from "@/components/ui/button";
+import { Config } from "./config";
 import { NavItem } from "./nav-item";
 
 const navItems = [
@@ -10,21 +12,25 @@ const navItems = [
     sectionId: "skills",
   },
   {
-    label: "Contact",
-    sectionId: "contact",
-  },
-  {
     label: "CV",
     sectionId: "cv",
+  },
+  {
+    label: "Contact",
+    sectionId: "contact",
   },
 ];
 
 export const Navbar = () => {
   return (
-    <nav className="gap-8 hidden md:flex">
+    <nav className="gap-8 hidden md:flex items-center font-medium">
       {navItems.map((item) => (
         <NavItem key={item.label} {...item} />
       ))}
+      <Button className="rounded-full border-2 border-background bg-emerald-400 hover:bg-background hover:text-emerald-400 hover:border-2 hover:border-emerald-400 cursor-pointer">
+        Projetos
+      </Button>
+      <Config />
     </nav>
   );
 };
