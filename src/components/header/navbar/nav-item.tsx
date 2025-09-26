03c9b2a84676d37b9eef8a0a4c0de05804285df9
Diogo Louvo Pievan <diogo.pievan@gmail.com>
2025-09-26 16:50:20 -0300

    fix: offset is a optional prop now

--- src/components/header/navbar/nav-item.tsx
+++ src/components/header/navbar/nav-item.tsx
@@ -3,16 +3,17 @@ import { Link } from "react-scroll";
 type NavItemProps = {
   label: string;
   sectionId: string;
+  offset?: number;
 };
 
-export const NavItem = ({ label, sectionId }: NavItemProps) => {
+export const NavItem = ({ label, sectionId, offset = 0 }: NavItemProps) => {
   return (
     <Link
       activeClass="text-emerald-400 border-b-2 border-emerald-400"
       to={sectionId}
       spy={true}
       smooth={true}
-      offset={10}
+      offset={offset}
       duration={800}
       className={
         "hover:text-emerald-400 transition-colors font-medium cursor-pointer"
