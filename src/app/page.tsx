982c8262e70b6695b22808552ec1a3346c3e3ef5
Diogo Louvo Pievan <diogo.pievan@gmail.com>
2025-09-26 09:48:20 -0300

    feat: skills section init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { AboutSection } from "@/components/about-section";
 import { HeroSection } from "@/components/hero-section";
+import { SkillsSection } from "@/components/skills-section";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
       <AboutSection />
+      <SkillsSection />
     </>
   );
 }
