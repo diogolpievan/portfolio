9f531db50c185dd967a6298fd5acd9247c375aa7
Diogo Louvo Pievan <diogo.pievan@gmail.com>
2025-09-29 14:19:30 -0300

    style: add py-8 in footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -3,7 +3,7 @@ import { Logo } from "../logo";
 export const Footer = () => {
   return (
     <footer className="flex justify-items-center w-full ">
-      <div className="container w-full flex flex-col md:flex-row justify-center items-center md:justify-between xl:px-40">
+      <div className="container py-8 w-full flex flex-col md:flex-row justify-center items-center md:justify-between xl:px-40">
         <Logo />
         <div className="text-[#666666] mt-2 md:mt-0">
           <p>&lt; Projeto por Diogo Louvo Pievan /&gt;</p>
