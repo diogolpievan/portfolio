c2c7329b5bf137d2e6528f2621be3e867eb6bc58
Diogo Louvo Pievan <diogo.pievan@gmail.com>
2025-10-01 09:39:30 -0300

    feat: profile links component

---
+++ src/components/profile-link/index.tsx
@@ -0,0 +1,27 @@
+import { cn } from "@/lib/utils";
+import { IconType } from "react-icons";
+
+type ProfileLinkProps = {
+  icon: IconType;
+  url: string;
+  size: number;
+  className?: string;
+};
+
+export const ProfileLink = ({
+  icon: Icon,
+  url,
+  size,
+  className,
+}: ProfileLinkProps) => {
+  return (
+    <a
+      href={url}
+      className="transition-all text-[#3b3b3b] duration-1000 hover:text-emerald-400 hover:-translate-y-4 text-shadow-neon cursor-pointer"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <Icon size={size} className={className} />
+    </a>
+  );
+};
